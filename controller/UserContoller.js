2eab21a938eb5a0f38475b70a3eec6ea4c575732
Calvin Danyalson <calvindany1102@gmail.com>
2023-06-27 12:07:54 +0700

    Finishing product-page design and add cart-page design

--- controller/UserContoller.js
+++ controller/UserContoller.js
@@ -1,7 +1,19 @@
+const Barang = require('../model/barang');
+
 exports.getLanding = (req, res, next) => {
     res.render('user/landing');
 }
 
-exports.getProduct = (req, res, next) => {
-    res.render('user/product')
+exports.getProduk = (req, res, next) => {
+    Barang.find()
+    .select('namabarang stok harga image')
+    .then( barang => {
+        res.render('user/produk',{
+            barang: barang,
+        })
+    })
+}
+
+exports.getKeranjang = (req, res, next) => {
+    res.render('user/keranjang');
 }
